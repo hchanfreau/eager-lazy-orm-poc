4a92c9c6d842667693641eb29d3b90db7423a8ce
Hernán Chanfreau <hchanfreau@gmail.com>
2025-11-27 17:19:32 -0300

    fix Dockerfile

--- laravel-eloquent-poc/entrypoint.sh
+++ laravel-eloquent-poc/entrypoint.sh
@@ -4,16 +4,20 @@
 set -e
 
 # Copy .env.example to .env if .env does not exist
+echo "STARTING: .env file check"
 if [ ! -f .env ]; then
     echo "Creating .env file from .env.example"
     cp .env.example .env
 fi
+echo "FINISHED: .env file check"
 
 # Run composer install
-echo "Running composer install..."
+echo "STARTING: composer install"
 composer install --no-interaction --prefer-dist --optimize-autoloader
+echo "FINISHED: composer install"
 
 # Generate application key if it's not set
+echo "STARTING: application key check"
 KEY_EXISTS=$(grep -c "APP_KEY=" .env)
 if [ "$KEY_EXISTS" -eq 0 ]; then
     echo "Generating application key..."
@@ -21,10 +25,18 @@ if [ "$KEY_EXISTS" -eq 0 ]; then
 else
     echo "Application key already exists."
 fi
+echo "FINISHED: application key check"
 
 # Run database migrations
-echo "Running database migrations..."
+echo "STARTING: database migrations"
 php artisan migrate --force
+echo "FINISHED: database migrations"
+
+# Set appropriate permissions for Laravel
+echo "STARTING: setting permissions for Laravel directories"
+chown -R laradock:www-data storage bootstrap/cache
+chown -R laradock:www-data database
+echo "FINISHED: setting permissions for Laravel directories"
 
 # Seed the database (optional, only if you have seeders)
 # echo "Seeding database..."
